64ec475a743c401414ae8c7955b3e138df93f58d
Kenaricoin <kenaricoinkn@gmail.com>
2025-09-19 01:17:10 +0700

    Create telegram.js

---
+++ js/telegram.js
@@ -0,0 +1,8 @@
+// Placeholder Telegram (bisa dipakai nanti untuk OAuth via bot)
+(function(){
+  const { $ } = window.App;
+  const tgFab = $('#tg-fab');
+  tgFab?.addEventListener('click', () => {
+    // nanti bisa ganti open bot / widget
+  });
+})();
