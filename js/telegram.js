1d4e407f6d447e0c7c72738e628e9315328994e0
Kenaricoin <kenaricoinkn@gmail.com>
2025-09-19 04:05:45 +0700

    Update and rename js/telegram.js to register.js

--- js/telegram.js
+++
@@ -1,8 +0,0 @@
-// Placeholder Telegram (bisa dipakai nanti untuk OAuth via bot)
-(function(){
-  const { $ } = window.App;
-  const tgFab = $('#tg-fab');
-  tgFab?.addEventListener('click', () => {
-    // nanti bisa ganti open bot / widget
-  });
-})();
